ca3d4784e5bb7627a37a804d93af637b4e3114ff
angelhd1999 <angelhd1999@gmail.com>
2022-10-09 19:50:03 +0200

    Update read_pickle.py

--- testing/read_pickle.py
+++ testing/read_pickle.py
@@ -1,10 +1,7 @@
 import pickle
 
 pickle_file = None
-try:
-    with open( '../qst1_w1/predictions/result.pkl', "rb" ) as f:
-        pickle_file = pickle.load(f)
-except:
-    pass
+with open( '../qsd2_w1/frames.pkl', "rb" ) as f:
+    pickle_file = pickle.load(f)
 
 print(pickle_file)
